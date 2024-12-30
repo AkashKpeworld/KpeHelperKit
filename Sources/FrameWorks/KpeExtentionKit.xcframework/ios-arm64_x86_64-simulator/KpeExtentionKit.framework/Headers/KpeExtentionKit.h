983c4e16439e8b0abb33a76b8858ddc2e479601f
AkashKpeworld <akashkpeworld@gmail.com>
2024-12-30 11:41:13 +0530

    - Add Extentions

---
+++ Sources/FrameWorks/KpeExtentionKit.xcframework/ios-arm64_x86_64-simulator/KpeExtentionKit.framework/Headers/KpeExtentionKit.h
@@ -0,0 +1,18 @@
+//
+//  KpeExtentionKit.h
+//  KpeExtentionKit
+//
+//  Created by KPEWORLD on 30/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KpeExtentionKit.
+FOUNDATION_EXPORT double KpeExtentionKitVersionNumber;
+
+//! Project version string for KpeExtentionKit.
+FOUNDATION_EXPORT const unsigned char KpeExtentionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KpeExtentionKit/PublicHeader.h>
+
+
--- Sources/KpeHelperKit/KpeHelperKit.swift
+++ Sources/KpeHelperKit/KpeHelperKit.swift
@@ -1,2 +1,4 @@
 import KpeHelperKit.KpeAPIHelper
 import KpeHelperKit.KpeRouteHelper
+import KpeHelperKit.KpePropertyWrapper
+import KpeHelperKit.KpeExtentionKit
